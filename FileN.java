84f2e638f8123269d999ae5bb6f518a906df973c
Muneebullah hussaini syed <149222230+muhsyed22@users.noreply.github.com>
2024-03-16 15:57:25 +0530

    scanner

---
+++ FileN.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class FileN{
+       public static void main(String[] p){
+       Scanner sc=new Scanner(System.in);
+       String name=sc.next();
+       System.out.println("welcome"+name);
+     }
+}
